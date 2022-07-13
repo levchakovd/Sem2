977da64e06fc91874631f225b6425b5745b6b56b
levchakovd <levchakovd@yandex.ru>
2022-07-13 23:56:56 +0300

    zad13

---
+++ zad13/Program.cs
@@ -0,0 +1,7 @@
+﻿int a;
+Console.WriteLine("Введите число, я скажу какая третья цифра ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a>99)
+    Console.Write(Convert.ToString(a)[2]);
+else
+    Console.WriteLine("третьей цифры нет");
