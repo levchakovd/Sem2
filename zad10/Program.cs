3766a4cd897cf4952399e5ef0aa81d9e6316435a
levchakovd <levchakovd@yandex.ru>
2022-07-13 23:50:40 +0300

    dz10

--- zad10/Program.cs
+++ zad10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+Console.WriteLine("Введи трехзначное число - ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a>=100 && a<=999)
+{
+        int result = (a/10%10);
+        Console.WriteLine(result);
+}
+else
+{
+        Console.WriteLine("Внимательнее! ТРЕХЗНАЧНОЕ число! ");
+    
+}
