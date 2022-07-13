00b30a49716661c93f9a98efb301dabdb32ec28b
levchakovd <levchakovd@yandex.ru>
2022-07-14 00:01:06 +0300

    zad15

---
+++ zad15/Program.cs
@@ -0,0 +1,13 @@
+﻿int n;
+n = Convert.ToInt32(Console.ReadLine());
+if (n >= 1 && n <= 7)
+{
+    if (n <=5)
+        Console.WriteLine("нет");
+    if (n >5)
+        Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет такого дня недели");
+}
