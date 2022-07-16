373d62602eb24311f560bea266159fd4bfbf59c0
levchakovd <levchakovd@yandex.ru>
2022-07-16 22:52:17 +0300

    I win

--- bank/Program.cs
+++ bank/Program.cs
@@ -1,14 +1,14 @@
-﻿int a,b,c;
+﻿double a,b,c;
 Console.WriteLine("Ваша стартовая сумма - ");
-a = Convert.ToInt32(Console.ReadLine());
+a = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Желаемая сумма - ");
-b = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Процент годовых- ");
-c = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToDouble(Console.ReadLine());
 
 double preresult,pre;
-Convert.ToDouble(preresult = 1+c/100);
-Convert.ToDouble(pre = (b/a));
+preresult = 1+c/100;
+pre = (b/a);
 int result=Convert.ToInt32(Math.Log(pre,preresult));
 
 Console.WriteLine(result);
