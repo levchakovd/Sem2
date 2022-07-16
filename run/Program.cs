ab6e49a6f7d354d1733fb0db7dabae2cc04e4ce3
levchakovd <levchakovd@yandex.ru>
2022-07-16 22:49:37 +0300

    Rungit add run/

---
+++ run/Program.cs
@@ -0,0 +1,14 @@
+﻿int b,c;
+double a;
+Console.WriteLine("Введи первое число - ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введи второе число - ");
+b = Convert.ToInt32(Console.ReadLine());
+c=1;
+
+while (a<b)
+{
+    a=a*1.15;
+    c++;
+}
+Console.WriteLine(c);
